b2a66a6982988e4b37efa9532151823cd633e15f
Pedro Resende <pedroluizrnunez@gmail.com>
2023-01-27 17:42:39 -0300

    inicital commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Recipes from './pages/Recipes';
 import Profile from './pages/Profile';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import DoneRecipes from './pages/DoneRecipes';
+import RecipeDetails from './pages/RecipeDetails';
 
 function App() {
   return (
@@ -17,6 +18,8 @@ function App() {
       <Route exact path="/profile" component={ Profile } />
       <Route exact path="/done-recipes" component={ DoneRecipes } />
       <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      <Route exact path="/meals/:id" component={ RecipeDetails } />
+      <Route exact path="/drinks/:id" component={ RecipeDetails } />
     </Switch>
   );
 }

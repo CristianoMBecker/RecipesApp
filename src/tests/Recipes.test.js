a381d3293b57a73e60c0c73700af5f287627b850
Pedro Resende <pedroluizrnunez@gmail.com>
2023-01-27 12:39:12 -0300

    atinge cobertura de 100% nos testes da págiga Recipes

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 import mealCategories from '../../cypress/mocks/mealCategories';
 import meals from '../../cypress/mocks/meals';
+import drinks from '../../cypress/mocks/drinks';
+import drinkCategoris from '../../cypress/mocks/drinkCategories';
 
 describe('test the application', () => {
-  // beforeEach(() => {
-  //   global.fetch = jest.fn().mockResolvedValue({
-  //     json: jest.fn().mockResolvedValue(meals),
-  //   });
-  // });
-
-  test.only('if the table renders correctly.', async () => {
-    // jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-    //   ok: 'true',
-    //   json: jest.fn().mockResolvedValue(meals),
-    // });
-
-    // jest.spyOn(global, 'fetch').mockResolvedValueOnce({
-    //   ok: 'true',
-    //   json: jest.fn().mockResolvedValue(mealCategories),
-    // });
+  it('if the table renders correctly.', async () => {
     global.fetch = jest.fn().mockImplementation((url) => {
       switch (url) {
       case 'https://www.themealdb.com/api/json/v1/1/search.php?s=':
@@ -40,18 +27,12 @@ describe('test the application', () => {
       }
     });
 
-    renderWithRouter(<App />);
+    const { history } = renderWithRouter(<App />);
 
-    const emailInput = screen.getByRole('textbox');
-    const passwordInput = screen.getByTestId('password-input');
-    const enterBtn = screen.getByRole('button', {
-      name: /enter/i,
+    act(() => {
+      history.push('/meals');
     });
 
-    userEvent.type(emailInput, 'test@yahoo.fr');
-    userEvent.type(passwordInput, '1234567');
-
-    userEvent.click(enterBtn);
     expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/search.php?s=');
     expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
     expect(global.fetch).toHaveBeenCalledTimes(2);
@@ -62,5 +43,51 @@ describe('test the application', () => {
       name: /goat/i,
     });
     expect(goatBtn).toBeInTheDocument();
+
+    const nameEl = await screen.findByRole('heading', { name: /corba/i });
+    const nameEl2 = await screen.findByRole('heading', { name: /big mac/i });
+
+    expect(nameEl).toBeInTheDocument();
+    expect(nameEl2).toBeInTheDocument();
+  });
+  test('se a página Recipes é renderizada corretamente com a rota "/drinks"', async () => {
+    global.fetch = jest.fn().mockImplementation((url) => {
+      switch (url) {
+      case 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=':
+        return Promise.resolve({
+          ok: true,
+          json: jest.fn().mockResolvedValue(drinks),
+        });
+      case 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list':
+        return Promise.resolve({
+          ok: true,
+          json: jest.fn().mockResolvedValue(drinkCategoris),
+        });
+      default:
+        return {};
+      }
+    });
+    const { history } = renderWithRouter(<App />);
+
+    act(() => {
+      history.push('/drinks');
+    });
+    expect(history.location.pathname).toBe('/drinks');
+
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+    expect(global.fetch).toHaveBeenCalledWith('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    expect(global.fetch).toHaveBeenCalledWith('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+
+    const ordinaryDrinkBtn = await screen.findByRole('button', { name: /ordinary drink/i });
+    const cocoaBtn = await screen.findByRole('button', { name: /cocoa/i });
+
+    expect(ordinaryDrinkBtn).toBeInTheDocument();
+    expect(cocoaBtn).toBeInTheDocument();
+
+    const nameEl = await screen.findByRole('heading', { name: /gg/i });
+    const nameEl2 = await screen.findByRole('heading', { name: /gg/i });
+
+    expect(nameEl).toBeInTheDocument();
+    expect(nameEl2).toBeInTheDocument();
   });
 });

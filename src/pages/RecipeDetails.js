b678c77045a912c8d5bdd094d8403ccdb2068349
Cristiano <crisbecker50@gmail.com>
2023-01-30 14:41:22 -0300

    req24

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import DetailsCard from '../components/DetailsCard';
 
-function RecipeDetails() {
+function RecipeDetails(props) {
+  const { history, match: { params: { id } } } = props;
+  const [response, setResponse] = useState([]);
+  const { location: { pathname } } = history;
+
+  const fetchRecipe = async () => {
+    if (pathname.includes('meals')) {
+      const data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const dataResponse = await data.json();
+      console.log(dataResponse);
+      setResponse(dataResponse);
+    } if (pathname.includes('drinks')) {
+      const data = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+      const dataResponse = await data.json();
+      console.log(dataResponse);
+      setResponse(dataResponse);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecipe();
+  }, []);
+
   return (
-    <div>RecipeDetails</div>
+    <main>
+      <p>{ response.title }</p>
+    </main>
   );
 }
+
+RecipeDetails.propTypes = {}.isRequired;
 
 export default RecipeDetails;

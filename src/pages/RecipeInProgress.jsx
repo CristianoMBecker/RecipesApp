4451feb5a2926de89209f01fb2556aa897e9deb9
Pedro Resende <pedroluizrnunez@gmail.com>
2023-02-01 16:18:08 -0300

    requisito 38 completo

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,25 +1,66 @@
-import React, { useContext } from 'react';
-import RecipesContext from '../context/RecipesContext';
+import React, { useEffect, useState } from 'react';
+import useFetch from '../hooks/useFetch';
 
 function RecipeInProgress({ history }) {
-  const { recipeApi } = useContext(RecipesContext);
+  const { makeFetch } = useFetch();
+  const [recipeApi, setRecipeApi] = useState([{}]);
   const { location: { pathname } } = history;
-  console.log(pathname.includes('meals'));
+  const id = pathname.split('/')[2];
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      if (pathname.includes('drinks')) {
+        const data = await makeFetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+        console.log(data);
+        setRecipeApi(data.drinks);
+      } else {
+        const data = await makeFetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+        console.log(data);
+        setRecipeApi(data.meals);
+      }
+    };
+    fetchRecipe();
+  }, []);
+  console.log(recipeApi);
+  const ing = (Object.entries(recipeApi[0])
+    .filter(([key, value]) => key.startsWith('strIngredient') && value)
+    .map(([, value]) => value));
+
+  const measures = (Object.entries(recipeApi[0])
+    .filter(([key, value]) => key.startsWith('strMeasure') && value)
+    .map(([, value]) => value));
+
+  const ingredientsAndCups = ing.map((item, index) => `${item} ${measures[index]}`);
+
   return (
     <div>
 
       in progress
       <h1 data-testid="recipe-title">
 
-        {pathname.includes('drinks') ? recipeApi.strDrink : recipeApi.strMeal}
+        {pathname.includes('drinks') ? recipeApi[0].strDrink : recipeApi[0].strMeal}
 
       </h1>
       <img
         src={ pathname.includes('drinks')
-          ? recipeApi.strDrinkThumb : recipeApi.strMealThumb }
-        alt={ pathname.includes('drinks') ? recipeApi.strDrink : recipeApi.strMeal }
+          ? recipeApi[0].strDrinkThumb : recipeApi[0].strMealThumb }
+        alt={ pathname.includes('drinks') ? recipeApi[0].strDrink : recipeApi[0].strMeal }
         data-testid="recipe-photo"
       />
+      <ul>
+        {
+          ingredientsAndCups.map((ingredient, index) => (
+            <div key={ `${ingredient}-${index}` }>
+              <label
+                data-testid="ingredient-step"
+                htmlFor={ `${index}-ingredient-step` }
+              >
+                <input id={ `${index}-ingredient-step` } type="checkbox" />
+                {ingredient}
+              </label>
+            </div>
+          ))
+        }
+      </ul>
       <button data-testid="share-btn">Share</button>
       <button data-testid="favorite-btn">Favorite</button>
       {

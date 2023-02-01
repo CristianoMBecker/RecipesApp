7ed977843307acd844f276d5ed315e80c75b729a
Pedro Resende <pedroluizrnunez@gmail.com>
2023-02-01 11:56:34 -0300

    css da Login completo

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import useForm from '../hooks/useForm';
 import saveOnLocalStorage from '../services/localStorage';
+import logoRecipesApp from '../images/logoRecipesApp.svg';
+import tomate from '../images/tomate.svg';
+import './Login.css';
 
 function Login({ history }) {
   const emailInput = useForm('');
@@ -27,45 +30,54 @@ function Login({ history }) {
     saveOnLocalStorage('user', { email: emailInput.value });
   };
   return (
-    <form>
-      <label htmlFor="email-input">
-        <input
-          id="email-input"
-          type="email"
-          data-testid="email-input"
-          value={ emailInput.value }
-          onChange={ (e) => {
-            emailInput.handleChange(e);
-            validateLogin();
+    <>
+      <div className="logo-content">
+        <img className="login-logo" src={ logoRecipesApp } alt="logo recipe app" />
+        <img className="tomate-image" src={ tomate } alt="tomates" />
+      </div>
+      <form className="form-login">
+        <h1>Login</h1>
+        <label htmlFor="email-input">
+          <input
+            id="email-input"
+            type="email"
+            data-testid="email-input"
+            value={ emailInput.value }
+            onChange={ (e) => {
+              emailInput.handleChange(e);
+              validateLogin();
+            } }
+            placeholder="Email"
+          />
+        </label>
+        <label htmlFor="password-input">
+          <input
+            id="password-input"
+            type="password"
+            data-testid="password-input"
+            value={ passwordInput.value }
+            onChange={ (e) => {
+              passwordInput.handleChange(e);
+              validateLogin();
+            } }
+            placeholder="Password"
+          />
+        </label>
+        <button
+          id=""
+          data-testid="login-submit-btn"
+          type="button"
+          disabled={ isDisabled }
+          onClick={ () => {
+            saveUser();
+            history.push('/meals');
           } }
-        />
-      </label>
-      <label htmlFor="password-input">
-        <input
-          id="password-input"
-          type="password"
-          data-testid="password-input"
-          value={ passwordInput.value }
-          onChange={ (e) => {
-            passwordInput.handleChange(e);
-            validateLogin();
-          } }
-        />
-      </label>
-      <button
-        id=""
-        data-testid="login-submit-btn"
-        type="button"
-        disabled={ isDisabled }
-        onClick={ () => {
-          saveUser();
-          history.push('/meals');
-        } }
-      >
-        Enter
+        >
+          Enter
 
-      </button>
-    </form>
+        </button>
+      </form>
+    </>
   );
 }
 

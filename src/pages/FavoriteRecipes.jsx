b07cc3bf5b9a1ffa54f785c759ea8ed7aa81a2da
Pedro Resende <pedroluizrnunez@gmail.com>
2023-02-04 02:25:31 -0300

    finaliza todos os requisitos da página de Receitas Favoritas do 50 ao 56

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import FavoriteCard from '../components/FavoriteCard';
 import Header from '../components/Header';
+import RecipesContext from '../context/RecipesContext';
 
 function FavoriteRecipes() {
+  const { favoriteRecipes, setFavoriteRecipes } = useContext(RecipesContext);
+  const [copiedCardIndex, setCopiedCardIndex] = useState(null);
+
+  const getFavorites = () => {
+    const favoritesInLocalStorage = JSON.parse(localStorage
+      .getItem('favoriteRecipes'));
+
+    setFavoriteRecipes(favoritesInLocalStorage);
+  };
+
+  useEffect(() => {
+    getFavorites();
+  }, []);
+
+  const handleShareClick = (index) => {
+    setCopiedCardIndex(index);
+  };
+
   return (
     <div>
       <Header title="Favorite Recipes" showSearch={ false } />
+      <button
+        onClick={ getFavorites }
+        data-testid="filter-by-all-btn"
+      >
+        All
+
+      </button>
+      <button
+        onClick={ () => setFavoriteRecipes(favoriteRecipes
+          .filter((r) => r.type === 'meal')) }
+        data-testid="filter-by-meal-btn"
+      >
+        Meals
+
+      </button>
+      <button
+        onClick={ () => setFavoriteRecipes(favoriteRecipes
+          .filter((r) => r.type === 'drink')) }
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+
+      </button>
+
+      {
+        favoriteRecipes.map((recipe, index) => (
+          <FavoriteCard
+            key={ recipe.id }
+            recipe={ recipe }
+            index={ index }
+            copiedCardIndex={ copiedCardIndex }
+            onShareClick={ handleShareClick }
+          />
+        ))
+      }
     </div>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-function Profile() {
+function Profile({ history }) {
+  const [email, setEmail] = useState('');
+  useEffect(() => {
+    const emailObj = JSON.parse(localStorage.getItem('user'));
+    setEmail(emailObj.email);
+  }, []);
+
   return (
     <div>
       <Header title="Profile" showSearch={ false } />
+      <p data-testid="profile-email">{ email }</p>
+      <button
+        onClick={ () => { history.push('/done-recipes'); } }
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+
+      </button>
+      <button
+        onClick={ () => { history.push('/favorite-recipes'); } }
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+
+      </button>
+      <button
+        onClick={ () => {
+          localStorage.clear();
+          history.push('/');
+        } }
+        data-testid="profile-logout-btn"
+      >
+        Logout
+
+      </button>
       <Footer />
     </div>
   );
 }
+
+Profile.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Profile;
